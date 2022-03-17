c08a0e34a16284d501410d1c6b1d387579ed360a
jinabro <kmy4869@naver.com>
2022-03-18 03:10:37 +0900

    ~프리팹

--- Repeat/Assets/_Game/Scripts/Managers/Managers.cs
+++ Repeat/Assets/_Game/Scripts/Managers/Managers.cs
@@ -8,7 +8,10 @@ public class Managers : MonoBehaviour
     private static Managers Instance { get { Init(); return s_instance; } }
 
     InputManager _input = new InputManager(); // 멤버 변수임.
+    ResourceManager _resource = new ResourceManager();
+    
     public static InputManager Input { get { return Instance._input; } } // 인풋사용하고싶으면 Managers.Input으로 사용하면 됨.
+    public static ResourceManager Resource { get { return Instance._resource; } }
 
     void Start()
     {
